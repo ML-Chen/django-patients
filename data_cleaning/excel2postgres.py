f6b31558e2056ac655701e26b5058a6c3b180f12
Michael Chen <michael.ld.chen@gmail.com>
2019-08-20 21:16:11 -0400

    Improve excel2postgres.py performance 500×

--- data_cleaning/excel2postgres.py
+++ data_cleaning/excel2postgres.py
@@ -1,9 +1,9 @@
-from typing import Dict, Union
+import datetime
+from typing import Dict, Union, Tuple, List
 from collections import namedtuple
 import os
 import openpyxl
 import psycopg2
-import datetime
 
 
 # Converts a 1-based column number to its letter
@@ -33,18 +33,18 @@ def snake_case(s: str) -> str:
     return '_'.join(s.split(' ')).lower()
 
 
-database = psycopg2.connect(**{
+conn = psycopg2.connect(**{
     'dbname': 'postgres',
     'user': 'postgres',
     'password': os.environ.get('DB_PASSWORD'),
     'host': '127.0.0.1',
     'sslmode': 'prefer'
 })
-cursor = database.cursor()
+cursor = conn.cursor()
 
 TableProps = namedtuple('FileTableName', 'filename tablename fields')
 tableprops = [
-    TableProps('Patient', 'patient', ('last_name', 'first_name', 'dob', 'phone', 'phone_2', 'address', 'gender', 'downstairs')),
+    # TableProps('Patient', 'patient', ('last_name', 'first_name', 'dob', 'phone', 'phone_2', 'address', 'gender', 'downstairs')),
     TableProps('Checkups', 'glasses_prescription', ('last_name', 'first_name', 'dob', 'date', 'od', 'os', 'va_right', 'va_left', 'pd', 'cc', 'conj', 'sclera', 'tears', 'cornea', 'iris', 'antc', 'lll')),
     TableProps('Glasses', 'glasses', ('last_name', 'first_name', 'dob', 'date', 'brand', 'model', 'color', 'frame', 'lens', 'contact_lens', 'payment', 'additional_comments')),
     TableProps('Insurance', 'insurance', ('last_name', 'first_name', 'dob', 'insurance_id', 'insurance_id_2'))
@@ -54,25 +54,34 @@ for tup in tableprops:
     print(f'\nWorking on {tup.filename}...')
     wb = openpyxl.load_workbook(f'C:/Users/micha/Google Drive/Patients 8-16-19/{tup.filename}.xlsx')
     sheet = wb.worksheets[0]
+    headers: List[Tuple[str, str]] = [(colnum2str(col_num + 1), snake_case(header_cell.value)) for col_num, header_cell in enumerate(list(sheet.rows)[0])]
 
     for row in range(2, sheet.max_row + 1):
+        if row % 500 == 0:
+            print(row)
         row = str(row)
         # dict of column name to cell value in that row
-        vars: Dict[str, Union[str, None]] = {
-            snake_case(header_cell.value): str(sheet[colnum2str(col_num + 1) + row].value)
-                                           if sheet[colnum2str(col_num + 1) + row].value is not None
-                                           else ''
-            for col_num, header_cell in enumerate(list(sheet.rows)[0])
-        }
-        print(vars)
+        vars: Dict[str, Union[str, None]] = {}
+        for col, header in headers:
+            val: Union[str, float, datetime.datetime, None] = sheet[col + row].value
+            if val is None:
+                val = ''
+            elif type(val) == datetime.datetime:
+                val = str(val)
+            elif val == 0:
+                val = "NULL"
+            elif type(val) == str:
+                val = val.replace("'", "’")
+            vars[header] = val
 
         fields = tup.fields
         vals = [vars[field] for field in fields]
-        query = f"""INSERT INTO {tup.tablename} {str(fields).replace("'", "")} VALUES {str(vals).replace('[', '(').replace(']', ')')}"""
+        query = f"""INSERT INTO {tup.tablename} {str(fields).replace("'", "")} VALUES {str(vals).replace("'NULL'", "NULL").replace('[', '(').replace(']', ')')}"""
 
         cursor.execute(query)
 
+    conn.commit()
+
 cursor.close()
-database.commit()
-database.close()
+conn.close()
 
